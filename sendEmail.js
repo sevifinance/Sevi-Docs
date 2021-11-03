467f5b80baf98277b863ca289ce77636dcdb396a
grinono <bartel_verkruijssen@hotmail.com>
2021-11-03 15:35:43 +0100

    create email from blog

--- sendEmail.js
+++ sendEmail.js
@@ -5,26 +5,35 @@ import { Remarkable } from 'remarkable';
 
 var md = new Remarkable();
 
-async function webhook() {
+const fileToCreateEmailFrom = './blog/2021-11-03-welcome.md'
 
-    fs.readFile('./blog/2021-11-03-welcome.md', 'utf8', async (err, data) => {
+async function webhook() {
+    fs.readFile(fileToCreateEmailFrom, 'utf8', async (err, data) => {
         const noheader = data.split("---")
         const markdownToHTML = md.render(noheader[2])
-        const emailTemplate = templateNewVersion
-            .replace("{markdownText}", markdownToHTML)
-            .replace("{title}", "Sevi App release version 0.9.0")
-            .replace(/\n/g, '')
-            .replace(/"/g, "'")
-        console.log('emailTemplate:', emailTemplate)
 
-        const body = { customHtml: emailTemplate }
-        // const urlFolder = process.node.NODE_ENV === 'development' ? "webhook-test" : "webhook"
-        const response = await fetch(`https://n8n.sevi.io/webhook-test/80985a34-def1-4a17-8486-0ec239b23b90`, {
-            method: 'post',
-            body: JSON.stringify(body),
-            headers: { 'Content-Type': 'application/json' }
+        fs.readFile('../Sevi-Mobile/package.json', async (err, data) => {
+            if (err)
+                console.log(err);
+            else {
+                var json = JSON.parse(data);
+                const emailTemplate = templateNewVersion
+                    .replace("{markdownText}", markdownToHTML)
+                    .replace("{title}", `Sevi App release ${json.version}`)
+                    .replace(/\n/g, '')
+                    .replace(/"/g, "'")
+
+                const body = { customHtml: emailTemplate, subject: `Sevi Update > ${json.version}` }
+
+                const response = await fetch(`https://n8n.sevi.io/webhook/80985a34-def1-4a17-8486-0ec239b23b90`, {
+                    method: 'post',
+                    body: JSON.stringify(body),
+                    headers: { 'Content-Type': 'application/json' }
+                })
+            }
         })
-        // console.log('response:', response)
+
+
     });
 
 }
