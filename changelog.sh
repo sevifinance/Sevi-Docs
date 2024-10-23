a69f5c45e826abd3c1d5af103bd88b630b581b78
grinono <bartel_verkruijssen@hotmail.com>
2024-10-23 15:19:12 +0200

    feat: some docs test

--- changelog.sh
+++
@@ -1,9 +0,0 @@
-cd /Users/bartel/Documents/development/Sevi-Docs/ &&
-cp '/Users/bartel/Documents/development/Sevi-Mobile/CHANGELOG.md' ./docs/changelog.md &&
-echo "---
-sidebar_position: 10
----
-\n$(cat ./docs/changelog.md )" > ./docs/changelog.md &&
-git add . &&
-git commit -m "new changelog" &&
-git push
